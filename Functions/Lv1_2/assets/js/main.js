db556e6c1b776e38859591f24384a2ca814e3b4b
Julia T <juliaturkali@gmail.com>
2024-12-10 13:49:54 +0100

    Task Functions Level 1_2

---
+++ Functions/Lv1_2/assets/js/main.js
@@ -0,0 +1,8 @@
+function intro2(paramName) {
+  let name = "SuperCoder:in";
+  console.log(`Hi ${name}. Mein Name ist ${paramName}.`);
+}
+
+intro2("Julia Turkali");
+intro2("Max Mustermann");
+intro2("Mareike Musterfrau");
