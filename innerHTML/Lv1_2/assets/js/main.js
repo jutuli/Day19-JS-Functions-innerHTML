f1d4a565e26bdbe7ce60f0133bce38fd9e5b4a69
Julia T <juliaturkali@gmail.com>
2024-12-10 16:17:25 +0100

    Fix innerHTML Lv1_2

--- innerHTML/Lv1_2/assets/js/main.js
+++ innerHTML/Lv1_2/assets/js/main.js
@@ -1,9 +1,7 @@
 let divContainer = document.getElementById("info");
 
-divContainer.innerHTML = "<h1>Hello World</h1>";
+divContainer.innerHTML += "<h1>Hello World</h1>";
 divContainer.innerHTML += "<h2>How are you</h2>";
-
-document.write("<div id='container'></div>");
 
 let secondContainer = document.getElementById("container");
 secondContainer.innerHTML = "<p>start of the element</p>";
