e31edc5c13933987fe13ebf8e606fdbfc9bfd5f1
Julia T <juliaturkali@gmail.com>
2024-12-10 13:54:56 +0100

    Task Functions Level1_3

---
+++ Functions/Lv1_3/.assets/js/main.js
@@ -0,0 +1,8 @@
+function intro3(name, stadt, alter) {
+  console.log(
+    `Hallo, mein Name ist ${name}. Ich bin ${alter} Jahre alt. Ich komme aus ${stadt}.`
+  );
+}
+
+intro3("Julia", "Berlin", 34);
+intro3("Ben", "Hamburg", "36");
