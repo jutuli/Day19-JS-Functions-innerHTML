279004705cc9c4d773de73e10731ca87f467849a
Julia T <juliaturkali@gmail.com>
2024-12-10 13:43:48 +0100

    innerHTML Task Lv 1_3

---
+++ innerHTML/Lv1_3/assets/js/main.js
@@ -0,0 +1,7 @@
+let figure = document.getElementById("gallery");
+figure.innerHTML =
+  "<figure><img src='https://fastly.picsum.photos/id/1079/400/300.jpg?hmac=LIhNOr0BHdCoIBDXPnv_qLfJdg5bAtv5GgD-yRskn_8' alt='Rundes Lichtspektakel'/> <figcaption>Fig 1</figcaption></figure>";
+figure.innerHTML +=
+  "<figure><img src='https://fastly.picsum.photos/id/5/400/300.jpg?hmac=p2XHhfq_gh3mLNRcXq_qJDUkQIxiuSVTb5GnLlsxPb0' alt='Mann arbeitet an Laptop'/> <figcaption>Fig 2</figcaption></figure>";
+figure.innerHTML +=
+  "<figure><img src='https://fastly.picsum.photos/id/1073/400/300.jpg?hmac=ZdJp4btme4xCGgSv2RS8ZmR5ht4Ky3wYyHl8kExsH9o' alt='Viele offene Bücher'/> <figcaption>Fig 3</figcaption></figure>";
